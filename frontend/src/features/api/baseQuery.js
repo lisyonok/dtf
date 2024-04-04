5fa1bd51fad117f804a02806c118219c36b4e3b2
Nikita <echkalov@e-krit.ru>
2024-04-04 22:21:36 +0300

    master – fix api url

--- frontend/src/features/api/baseQuery.js
+++ frontend/src/features/api/baseQuery.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query"
 
 export default fetchBaseQuery({
-  baseUrl: process.env.REACT_APP_API_URL,
+  baseUrl: process.env.REACT_APP_API_URL + "/api/",
   async responseHandler(response) {
     return await response.json()
   },
